4173d9e069cc918b2426e32460282b36f6d3a971
LiaoWenjun99 <liaowenjun1999@gmail.com>
2024-04-23 21:49:33 +0800

    Update RFC_Feature_Selection.py

--- Codes/ML/RFC_Feature_Selection.py
+++ Codes/ML/RFC_Feature_Selection.py
@@ -9,19 +9,16 @@
 import sys
 import pandas as pd
 import numpy as np
-
 import time
-
 from sklearn.metrics import classification_report
 
-print('Importing necessary python libraries...')
-
+###State number of features to select
 features = '100'
 title = 'RFC_Feature_Selection_' + features
-# # Sample  
 
-# In[ ]:
+# In[1]:
 
+###Import dataset (Datasets are assumed to be in same directory as this .py file)
 start_import_time = time.time() 
 samples_dataframe_H = pd.read_csv('data_H.csv',names = ['0'])
 samples_dataframe_M = pd.read_csv('data_M.csv',names = ['0'])
@@ -30,16 +27,14 @@ end_import_time = time.time()
 import_time = end_import_time-start_import_time
 print('Importing time =', import_time)
 
+### Import amino acid datasets
 #samples_dataframe_H = pd.read_csv('data_AA_H.csv',names = ['0'])
 #samples_dataframe_M = pd.read_csv('data_AA_M.csv',names = ['0'])
 #samples_dataframe_L = pd.read_csv('data_AA_L.csv',names = ['0'])
 
-# define number of samples to extract
+### define number of samples to extract randomly
+### and remove entire rows with any 'N' in it
 num_random_rows = 420000
-#H_rand = samples_dataframe_H[0:num_random_rows]
-#M_rand = samples_dataframe_M[0:num_random_rows]
-#L_rand = samples_dataframe_L[0:num_random_rows]
-
 
 H_rand = samples_dataframe_H.sample(n=num_random_rows)
 H_rand = H_rand[~H_rand['0'].str.contains('N')]
@@ -50,8 +45,12 @@ M_rand = M_rand[~M_rand['0'].str.contains('N')]
 L_rand = samples_dataframe_L.sample(n=num_random_rows)
 L_rand = L_rand[~L_rand['0'].str.contains('N')]
 samples_all = pd.concat([H_rand, M_rand, L_rand])
+
+### Use these for 2 classes training
 #samples_all = pd.concat([M_rand, L_rand])
 #samples_all = pd.concat([M_rand, L_rand])
+
+### Print the number of rows in each variable
 print('High')
 print(len(H_rand),'\n')
 print('Medium')
@@ -61,37 +60,34 @@ print(len(L_rand),'\n')
 
 
 
-# In[ ]:
+# In[2]:
 
-# Selecting column '0' and splitting it into columns 
+### Selecting column '0' and splitting it into columns 
 samples_all = samples_all['0'].str.split('',expand=True)
-# Removing the first col of 0 followed by last col
+### Removing the first col of 0 followed by last col
 samples_all = samples_all.iloc[:,1:]
 samples_all.drop(columns=samples_all.columns[-1],axis=1,inplace=True)
 
+### Save the file if needed
 #samples_all.to_csv('samples_RandomForest_all.csv')
 
-
-# In[11]:
-
-
-#samples_all = samples_all.loc[:,83:249]
-
-
-# In[5]:
+# In[3]:
 
 start_ohe_time = time.time()
 
-# One hot encoding on all columns to separate into e.g 3_C,4_T, 4_G, 5_A
+### One hot encoding on all columns to separate into e.g 3_C,4_T, 4_G, 5_A
 H_ohe = pd.get_dummies(samples_all)
-# Creating labels for H,M,L
+### Creating labels for H,M,L
 H_perflist_H = [0]*len(H_rand)
 H_perflist_M = [1]*len(M_rand)
 H_perflist_L = [2]*len(L_rand)
 H_perflist = H_perflist_H + H_perflist_M + H_perflist_L
+
+### Use for 2 classes training
 #H_perflist = H_perflist_H + H_perflist_L
 #H_perflist = H_perflist_M + H_perflist_L 
 
+### Convert to numpy array so as to concat with OHE data
 H_perflist = np.array(H_perflist)
 df_temp = pd.DataFrame(H_perflist, columns=['class'], dtype = 'uint8', index = H_ohe.index)
 H_ohe = pd.concat([H_ohe,df_temp],axis = 1)
@@ -100,20 +96,17 @@ end_ohe_time = time.time()
 ohe_time = end_ohe_time-start_ohe_time
 print('OHE time =', ohe_time)
 
-# H_ohe['class'] = H_perflist.flatten().tolist()
+### Save a sample of the OHE data so trained model can have a reference
+### to predict new sequences (New sequence must have the same column names)
+### as trained model)
 all_ohe = H_ohe
 sample = all_ohe.loc[:,H_ohe.columns != 'class'] 
 sample = sample.sample()
 sample.to_csv('RFC_cols_OHE_WO_Feat_Sel_'+ features + '.csv', index=False)
 
-# In[14]:
+# In[4]:
 
-
-#H_ohe.to_csv('one hot encoding_RandomForest_all.csv')
-
-
-# In[6]:
-
+### Split the data into train and test sets
 
 from sklearn.model_selection import train_test_split
 
@@ -129,6 +122,7 @@ print('train test split: ',t_split_test_train)
 
 # In[7]:
 
+### Save the split data so that other experiments can use the same set if needed
 
 #X_train.to_csv('X_train_RandomForest_all.csv')
 #X_test.to_csv('X_test_RandomForest_all.csv')
@@ -138,6 +132,7 @@ print('train test split: ',t_split_test_train)
 
 # In[8]:
 
+### Import relevant models if needed
 
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import cross_validate
@@ -154,7 +149,7 @@ from sklearn.ensemble import RandomForestClassifier
 
 # In[10]:
 
-
+### Fitting the Machine Learning model
 start_RF_fit = time.time()
 
 clf_RFclass = RandomForestClassifier()
@@ -164,6 +159,7 @@ end_RF_fit = time.time()
 t_RF_fit = end_RF_fit - start_RF_fit
 print('Fit time: ',t_RF_fit)
 
+### Create a variable to save the parameters used to train this model if needed
 
 results_RF = {'total no. of samples': len(all_ohe.axes[0]),
            'total no. of features': len(all_ohe.axes[1]), \
@@ -181,6 +177,8 @@ results_RF = {'total no. of samples': len(all_ohe.axes[0]),
 
 # In[22]:
 
+### Test model on test set
+
 start_Y_prediction_time = time.time()
 
 Y_pred = clf_RFclass.predict(X_test)
@@ -190,7 +188,7 @@ end_Y_prediction_time = time.time()
 Y_prediction_time = end_Y_prediction_time - start_Y_prediction_time
 print('Y_prediction_time: ',Y_prediction_time)
 
-#report
+### Save model's predictions (includes precision, recall, accuracy)
 
 report_df = pd.DataFrame(report)
 report_df.to_csv('Classification_report_WO_Feat_Sel_' + features + '.csv')
@@ -200,6 +198,7 @@ report_df.to_csv('Classification_report_WO_Feat_Sel_' + features + '.csv')
 
 # In[34]:
 
+### Plot confusion matrix if needed
 
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import ConfusionMatrixDisplay
@@ -216,6 +215,7 @@ import matplotlib.pyplot as plt
 
 # In[1]:
 
+### Generate accuracy scores if needed
 
 from sklearn.metrics import accuracy_score
 ##
@@ -229,15 +229,14 @@ from sklearn.metrics import accuracy_score
 
 # In[ ]:
 
-##
+### Perform cross validation if needed
+
 ##cv_scores_logis = cross_val_score(clf_RFclass,X_train,Y_train,cv=5)
 ##cv_scores_mean_logis = cv_scores_logis.mean()
 ##cv_scores_sd_logis = cv_scores_logis.std()
 
+### Save the cross validation parameters if needed
 
-# In[ ]:
-
-##
 ##others_RF = {
 ##           'clf score test':score_clf_test,\
 ##    'clf score train': score_clf_train,\
@@ -263,12 +262,16 @@ from sklearn.metrics import accuracy_score
 
 # In[ ]:
 
+### Feature Importance Code
+
 start_feat_impt_time = time.time()
 
+### Generate feature importance values for each feature and save them
 feat_imp = clf_RFclass.feature_importances_
 feat_imp_df = pd.DataFrame(feat_imp)
 feat_imp_df.to_csv('feat_imp_RandomForest_' + features + '.csv')
 
+### Sort the features in descending order of feature importance
 s = sorted(enumerate(feat_imp), key=lambda x: x[1], reverse= True)
 extract_top_features = []
 for i in range(0,int(features)):
@@ -277,9 +280,11 @@ extract_top_features.append(len(H_ohe.columns)-1)
 all_ohe_extract = H_ohe
 all_ohe_extracted = all_ohe_extract.iloc[:, extract_top_features]
 
+### Assign X to features and Y to class
 input_X2 = all_ohe_extracted.loc[:,all_ohe_extracted.columns != 'class'] 
 input_Y2 = all_ohe_extracted['class']
 
+### Save a sample for trained model to reference with
 all_ohe2 = all_ohe_extracted
 sample2 = all_ohe2.loc[:,all_ohe_extracted.columns != 'class'] 
 sample2 = sample2.sample()
@@ -289,20 +294,17 @@ end_feat_impt_time = time.time()
 feat_impt_time = end_feat_impt_time - start_feat_impt_time
 print('Feature importance sorting time: ',feat_impt_time)
 
+### Split into training and test for features selected
 
 start_split_test_train2 = time.time()
-
 X_train2, X_test2, Y_train2, Y_test2 = train_test_split(input_X2,input_Y2,test_size=0.3,random_state=0)
-
 end_split_test_train2 = time.time()
 t_split_test_train2 = end_split_test_train2 - start_split_test_train2
 print('train test split 2: ',t_split_test_train2)
 
+### Fit and save RFC to features selected data
 start_RF_fit2 = time.time()
-
-
 clf_RFclass2 = RandomForestClassifier()
-
 clf_RFclass2.fit(X_train2,Y_train2)
 
 
@@ -316,10 +318,9 @@ results_RF2 = {'total no. of samples': len(all_ohe_extracted.axes[0]),
            'train/test split':'70/30',\
            'train test split time':t_split_test_train2,\
            'fit time' : str(t_RF_fit2),\
-           
-           
           }
 
+### Test trained model with test set for features selected
 start_Y_prediction_time = time.time()
 
 Y_pred2 = clf_RFclass2.predict(X_test2)
@@ -332,6 +333,8 @@ print('Feature Importance', len(extract_top_features), 'prediction time: ',Y_pre
 report_df2 = pd.DataFrame(report2)
 report_df2.to_csv('Classification_report_'+ title +'.csv')
 
+
+### The codes below are for parallel feature selection if needed
 ### In[ ]:
 ##    
 ##extract_top_features = []
@@ -463,22 +466,6 @@ from sklearn.metrics import accuracy_score
 
 
 ####save the trained model
-
-##import joblib
-##
-##start_saving_model_time = time.time()
-##joblib.dump(clf_RFclass, "./RFC_ALL.joblib")
-##end_saving_model_time = time.time()
-##saving_model_time_wo_feature_impt = end_saving_model_time - start_saving_model_time
-##print("Saving model time without feat impt: ", saving_model_time_wo_feature_impt)
-##
-##
-##start_saving_model_time2 = time.time()
-##joblib.dump(clf_RFclass2, "./RFC_Feature_Selection_Top_1500.joblib")
-##end_saving_model_time2 = time.time()
-##saving_model_time_wth_1000_feature_impt = end_saving_model_time2 - start_saving_model_time2
-##print("Saving model time without feat impt: ", saving_model_time_wth_1000_feature_impt)
-##joblib.dump(clf_RFclass3, "./RFC_Feature_Selection_Top_2000.joblib")
 
 import pickle
 
